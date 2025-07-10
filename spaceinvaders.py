9ec79ef3f1a105aed57311f29d7247f469d3e08d
gunzblazincourier <gunzblazinAKM@proton.me>
2025-07-10 17:09:41 -0600

    Allow sideways movement of player

--- spaceinvaders.py
+++ spaceinvaders.py
@@ -8,14 +8,22 @@ spritesheet = pygame.image.load('sheet.png')
 player = pygame.Surface((14, 7))
 player.blit(spritesheet, (0, 0), (2, 49, 14, 7))
 player = pygame.transform.scale_by(player, 3)
-screen.blit(player, (640, 600))
+player_pos = pygame.Vector2(640, 600)
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        player_pos.x -= 300 * dt
+    elif keys[pygame.K_RIGHT]:
+        player_pos.x += 300 * dt
+
+    screen.fill((0, 0, 0))
+    screen.blit(player, (player_pos.x, player_pos.y))
     pygame.display.flip()
-    clock.tick(60)
+    dt = clock.tick(60) / 1000
 
 pygame.quit()
