30b78931b97db4840d1a422b76ef117435b850ad
gunzblazincourier <gunzblazinAKM@proton.me>
2025-07-14 07:56:50 -0600

    Declare and define variables just before being used instead of start of program

--- spaceinvaders.py
+++ spaceinvaders.py
@@ -3,42 +3,45 @@ import pygame
 pygame.init()
 SCREEN = pygame.display.set_mode((256, 224), pygame.SCALED | pygame.FULLSCREEN)
 clock = pygame.time.Clock()
-running = True
+spritesheet = pygame.image.load('sheet.png').convert()
 
 PLAYER_WIDTH = 16
 PLAYER_HEIGHT = 8
-
-ENEMY_WIDTH = 16
-ENEMY_HEIGHT = 8
-
-BULLET_WIDTH = 1
-BULLET_HEIGHT = 4
-bullet_shot = False
-
-linebreak_list = []
-enemy_list = []
-enemy_positions_list = []
-enemy_position_x = 50
-for i in range(11):
-    enemy_positions_list.append(pygame.Vector2(enemy_position_x, 70))
-    enemy_position_x += 14
-print(enemy_positions_list)
-
-spritesheet = pygame.image.load('sheet.png').convert()
 player = pygame.Surface((PLAYER_WIDTH, PLAYER_HEIGHT)).convert()
 player.blit(spritesheet, (0, 0), (1, 49, PLAYER_WIDTH, PLAYER_HEIGHT))
 player_position = pygame.Vector2(128, 200)
 
-for i in range(11):
-    enemy = pygame.Surface((ENEMY_WIDTH, ENEMY_HEIGHT)).convert()
-    #enemy.blit(spritesheet, (0, 0), (1, 1, ENEMY_WIDTH, ENEMY_HEIGHT))
-    enemy_list.append(enemy)
+enemy_rows, enemy_columns = (11, 5)
+enemy_positions_list =  [[player_position for i in range(enemy_columns)] for j in range(enemy_rows)]
+enemy_position_x = 50
+enemy_position_y = 70
+for i in range(enemy_columns):
+    for j in range(enemy_rows):
+        enemy_positions_list[j][i] = pygame.Vector2(enemy_position_x, enemy_position_y)
+        enemy_position_x += 14
+    enemy_position_x = 50
+    enemy_position_y += 10
+print(enemy_positions_list)
 
+ENEMY_WIDTH = 16
+ENEMY_HEIGHT = 8
+enemy_list = [[player for i in range(enemy_columns)] for j in range(enemy_rows)]
+for i in range(enemy_columns):
+    for j in range(enemy_rows):
+        enemy = pygame.Surface((ENEMY_WIDTH, ENEMY_HEIGHT)).convert()
+        enemy_list[j][i] = enemy
+print(enemy_list)
+
+BULLET_WIDTH = 1
+BULLET_HEIGHT = 4
 bullet = pygame.Surface((BULLET_WIDTH, BULLET_HEIGHT)).convert()
 bullet.blit(spritesheet, (0, 0), (55, 53, BULLET_WIDTH, BULLET_HEIGHT))
 gun_position = pygame.Vector2(136, 200)
 bullet_position = gun_position
 
+linebreak_list = []
+bullet_shot = False
+running = True
 while running:
     dt = clock.tick(144) / 1000
     for event in pygame.event.get():
@@ -64,15 +67,17 @@ while running:
     if bullet_shot:
         bullet_position.y -= 200 * dt
 
-    for x in enemy_list:
-        if int(pygame.time.get_ticks() / 1000) % 2 == 0:
-            x.blit(spritesheet, (0, 0), (1, 1, ENEMY_WIDTH, ENEMY_HEIGHT))
-        elif int(pygame.time.get_ticks() / 1000) % 2 == 1:
-            x.blit(spritesheet, (0, 0), (1, 11, ENEMY_WIDTH, ENEMY_HEIGHT))
+    for i in range(enemy_columns):
+        for j in range(enemy_rows):
+            if int(pygame.time.get_ticks() / 1000) % 2 == 0:
+                enemy_list[j][i].blit(spritesheet, (0, 0), (1, 1, ENEMY_WIDTH, ENEMY_HEIGHT))
+            elif int(pygame.time.get_ticks() / 1000) % 2 == 1:
+                enemy_list[j][i].blit(spritesheet, (0, 0), (1, enemy_rows, ENEMY_WIDTH, ENEMY_HEIGHT))
     SCREEN.fill((0, 0, 0))
     SCREEN.blit(player, (player_position.x, player_position.y))
-    for i in range(11):
-        SCREEN.blit(enemy_list[i], (enemy_positions_list[i].x, enemy_positions_list[i].y))
+    for i in range(enemy_columns):
+        for j in range(enemy_rows):
+            SCREEN.blit(enemy_list[j][i], (enemy_positions_list[j][i].x, enemy_positions_list[j][i].y))
     SCREEN.blit(bullet, (bullet_position.x, bullet_position.y))
 
     for i in range(28, 230):
