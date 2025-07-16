4ec9f4a8f3f53cb8cf12464e396ec930968ad69d
gunzblazincourier <gunzblazinAKM@proton.me>
2025-07-16 14:25:38 -0600

    Replace certain code with their function counterparts

--- spaceinvaders.py
+++ spaceinvaders.py
@@ -1,7 +1,4 @@
-from fileinput import filename
-
 import pygame
-from pygame import Surface
 
 
 def initialize_pygame_modules():
@@ -22,15 +19,11 @@ def draw_surface_on_surface(surface_top, surface_bottom, destination, area):
 def assign_coordinates(x, y):
     return pygame.Vector2(x, y)
 
-def set_enemy_direction(direction):
-    return direction
-
 def set_enemy_list_dimensions(rows, columns):
     return rows, columns
 
 def initialize_2d_list(value, rows, columns):
     return [[value for _ in range(columns)] for _ in range(rows)]
-
 
 def assign_coordinates_to_positions_list(positions, rows, columns, x, y):
     for i in range(rows):
@@ -51,7 +44,7 @@ def create_clock():
     return pygame.time.Clock()
 
 def set_fps(fps):
-    return clock.tick(fps)
+    return create_clock().tick(fps)
 
 def set_delta_time(fps):
     return set_fps(fps) / 1000
@@ -67,10 +60,10 @@ def check_quit_event(event):
 def get_all_keys_state():
     return pygame.key.get_pressed()
 
-def check_key_press(keys, key):
-    return keys[key]
+def check_key_press(key):
+    return get_all_keys_state()[key]
 
-def move_object_and_account_delta_time(object, speed, dt):
+def move_object_and_account_delta_time(object, speed: int, dt):
     object += speed * dt
     return object
 
@@ -97,94 +90,82 @@ def quit_game():
 
 
 
-#pygame.init()
-#SCREEN = pygame.display.set_mode((256, 224), pygame.SCALED)
+initialize_pygame_modules()
+SCREEN = initialize_display_window()
 
-PLAYER_WIDTH = 16
-PLAYER_HEIGHT = 8
-player = pygame.Surface((PLAYER_WIDTH, PLAYER_HEIGHT)).convert()
+player = create_surface(16, 8)
 
-spritesheet = pygame.image.load('sheet.png').convert()
-player.blit(spritesheet, (0, 0), (1, 49, PLAYER_WIDTH, PLAYER_HEIGHT))
-player_position = pygame.Vector2(128, 200)
+spritesheet = load_image('sheet.png')
+draw_surface_on_surface(spritesheet, player, (0, 0), (1, 49, 16, 8))
+player_position = assign_coordinates(128, 200)
 
 enemy_direction = True  #True = right; False = left
 previous_second = 0 #No function
 current_second = 0  #No function
-ENEMY_ROWS, ENEMY_COLUMNS = (5, 11)
+
+ENEMY_ROWS, ENEMY_COLUMNS = set_enemy_list_dimensions(5, 11)
+enemy_positions_list = initialize_2d_list(player_position, ENEMY_ROWS, ENEMY_COLUMNS)
+enemy_death_list = initialize_2d_list(False, ENEMY_ROWS, ENEMY_COLUMNS)
+enemy_timer_list = initialize_2d_list(1.00, ENEMY_ROWS, ENEMY_COLUMNS)
+
 
 # player_position assigned by default to set correct data type of Vector2 to values
-enemy_positions_list =  [[player_position for _ in range(ENEMY_COLUMNS)] for _ in range(ENEMY_ROWS)]
 
 # Stores which enemies are dead
-enemy_death_list = [[False for _ in range(ENEMY_COLUMNS)] for _ in range(ENEMY_ROWS)]
 
 # Dead enemies play death animation until timer runs out
-enemy_timer_list = [[1.00 for _ in range(ENEMY_COLUMNS)] for _ in range(ENEMY_ROWS)]
 
 # Helps assign positions to enemies by updating values in these variables during below loop
-enemy_position_x = 40
-enemy_position_y = 60
-for i in range(ENEMY_ROWS):
-    for j in range(ENEMY_COLUMNS):
-        enemy_positions_list[i][j] = pygame.Vector2(enemy_position_x, enemy_position_y)
-        enemy_position_x += 16
-    enemy_position_x = 40
-    enemy_position_y += 15
+assign_coordinates_to_positions_list(enemy_positions_list, ENEMY_ROWS, ENEMY_COLUMNS, 40, 60)
+
 
 ENEMY_WIDTH = 16
 ENEMY_HEIGHT = 8
-enemy_list = [[player for _ in range(ENEMY_COLUMNS)] for _ in range(ENEMY_ROWS)]
+enemy_list = initialize_2d_list(player, ENEMY_ROWS, ENEMY_COLUMNS)
 for i in range(ENEMY_ROWS):
     for j in range(ENEMY_COLUMNS):
-        enemy = pygame.Surface((ENEMY_WIDTH, ENEMY_HEIGHT)).convert()
+        enemy = create_surface(16, 8)
         enemy_list[i][j] = enemy
 
-BULLET_WIDTH = 1
-BULLET_HEIGHT = 4
-bullet = pygame.Surface((BULLET_WIDTH, BULLET_HEIGHT)).convert()
-bullet.blit(spritesheet, (0, 0), (55, 53, BULLET_WIDTH, BULLET_HEIGHT))
-gun_position = pygame.Vector2(136, 200)
+bullet = create_surface(1, 4)
+draw_surface_on_surface(spritesheet, bullet, (0, 0), (55, 53, 1, 4))
+gun_position = assign_coordinates(136, 200)
 bullet_position = gun_position  # 'Stores' bullet in gun of spaceship by default
-
-sound_shoot = pygame.mixer.Sound('shoot.wav')
-channel_shoot = pygame.mixer.Channel(1)
 
 # Row and column of the last enemy in the list when traversed column-by-column instead of the normal row-by-row looping
 # left-to-right list traversal for right travel direction, and right-to-left traversal for left direction
 # (default is for bottom-rightmost enemy)
 last_enemy_row = ENEMY_ROWS - 1
 last_enemy_column = ENEMY_COLUMNS - 1
-enemy_position_y = 0
 
 linebreak_list = [] # Line is broken at which locations
 bullet_shot = False
 game_running = True
-clock = pygame.time.Clock()
+create_clock()
+
+enemy_position_y = 0
 while game_running:
-    delta_time = clock.tick(144) / 1000
+    delta_time = set_delta_time(144)
     current_second = int(pygame.time.get_ticks() / 1000)    # No function
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            game_running = False
+    for event in get_events_from_queue():
+        game_running = check_quit_event(event)
 
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_SPACE] and bullet_shot == False:
-        bullet_position = pygame.Vector2(gun_position.x, gun_position.y)
+    if check_key_press(pygame.K_SPACE) and bullet_shot == False:
+        bullet_position = assign_coordinates(gun_position.x, gun_position.y)
         bullet_shot = True
-        channel_shoot.play(sound_shoot)
-    if keys[pygame.K_LEFT] and player_position.x > 30:
+        play_sound_from_channel('shoot.wav', 1)
+    if check_key_press(pygame.K_LEFT) and player_position.x > 30:
         player_position.x -= 100 * delta_time
         gun_position.x -= 100 * delta_time
-    elif keys[pygame.K_RIGHT] and player_position.x < 220:
+    elif check_key_press(pygame.K_RIGHT) and player_position.x < 220:
         player_position.x += 100 * delta_time
         gun_position.x += 100 * delta_time
 
     # Bullet destroys line where it hits
     if bullet_position.y < 15:
         bullet_shot = False
-        linebreak_list.append(int(bullet_position.x))
+        add_item_in_front_of_list(linebreak_list, int(bullet_position.x))
 
     if bullet_shot:
         bullet_position.y -= 200 * delta_time
@@ -192,7 +173,6 @@ while game_running:
         bullet_position = gun_position  #  Bullet respawns at gun_position
 
     # Updates coordinates of the last enemy when an enemy is killed, based on the direction of travel
-    # print(last_enemy_row, last_enemy_column)
     if enemy_direction:
         for j in range(ENEMY_COLUMNS):
             for i in range(ENEMY_ROWS):
@@ -250,58 +230,31 @@ while game_running:
             if enemy_death_list[i][j]:
                 if enemy_timer_list[i][j] > 0:
                     enemy_timer_list[i][j] -= 5 * delta_time    # Play dead enemy's timer for death animation
-                    enemy_list[i][j].blit(spritesheet, (0, 0), (55, 1, ENEMY_WIDTH, ENEMY_HEIGHT))
+                    draw_surface_on_surface(spritesheet, enemy_list[i][j], (0, 0), (55, 1, 16, 8))
                 else:
                     enemy_timer_list[i][j] = 0  # Death animation finished, enemy removed
             else:
-            #     if previous_second != current_second: # Means if one second has passed
-            #         if enemy_direction:
-            #             if enemy_positions_list[last_enemy_row][last_enemy_column].x < 220:
-            #                 enemy_positions_list[i][j].x += 3
-            #             else:
-            #                 if enemy_position_y != enemy_positions_list[last_enemy_row][last_enemy_column].y:
-            #                     enemy_positions_list[i][j].x += 3
-            #                     if (i == ENEMY_ROWS-1) and (j == ENEMY_COLUMNS-1):
-            #                         enemy_position_y = enemy_positions_list[last_enemy_row][last_enemy_column].y
-            #                 else:
-            #                     enemy_positions_list[i][j].y += 3
-            #                     if (i == ENEMY_ROWS-1) and (j == ENEMY_COLUMNS-1):
-            #                         enemy_direction = False
-            #                         enemy_position_y -= 2
-            #         else:
-            #             if enemy_positions_list[last_enemy_row][last_enemy_column].x > 40:
-            #                 enemy_positions_list[i][j].x -= 3
-            #             else:
-            #                 if enemy_position_y != enemy_positions_list[last_enemy_row][last_enemy_column].y:
-            #                     enemy_positions_list[i][j].x -= 3
-            #                     if (i == ENEMY_ROWS - 1) and (j == ENEMY_COLUMNS - 1):
-            #                         enemy_position_y = enemy_positions_list[last_enemy_row][last_enemy_column].y
-            #                 else:
-            #                     enemy_positions_list[i][j].y += 3
-            #                     if (i == ENEMY_ROWS - 1) and (j == ENEMY_COLUMNS - 1):
-            #                         enemy_direction = True
-            #                         enemy_position_y -= 2
 
                 # Animate enemies
                 if i == 0:
                     if current_second % 2 == 0:
-                        enemy_list[i][j].blit(spritesheet, (0, 0), (1, 1, ENEMY_WIDTH, ENEMY_HEIGHT))
+                        draw_surface_on_surface(spritesheet, enemy_list[i][j], (0, 0), (1, 1, 16, 8))
                     elif current_second % 2 == 1:
-                        enemy_list[i][j].blit(spritesheet, (0, 0), (1, 11, ENEMY_WIDTH, ENEMY_HEIGHT))
+                        draw_surface_on_surface(spritesheet, enemy_list[i][j], (0, 0), (1, 11, 16, 8))
                 elif i == 1 or i == 2:
                     if current_second % 2 == 0:
-                        enemy_list[i][j].blit(spritesheet, (0, 0), (19, 1, ENEMY_WIDTH, ENEMY_HEIGHT))
+                        draw_surface_on_surface(spritesheet, enemy_list[i][j], (0, 0), (19, 1, 16, 8))
                     elif current_second % 2 == 1:
-                        enemy_list[i][j].blit(spritesheet, (0, 0), (19, 11, ENEMY_WIDTH, ENEMY_HEIGHT))
+                        draw_surface_on_surface(spritesheet, enemy_list[i][j], (0, 0), (19, 11, 16, 8))
                 else:
                     if current_second % 2 == 0:
-                        enemy_list[i][j].blit(spritesheet, (0, 0), (37, 1, ENEMY_WIDTH, ENEMY_HEIGHT))
+                        draw_surface_on_surface(spritesheet, enemy_list[i][j], (0, 0), (37, 1, 16, 8))
                     elif current_second % 2 == 1:
-                        enemy_list[i][j].blit(spritesheet, (0, 0), (37, 11, ENEMY_WIDTH, ENEMY_HEIGHT))
+                        draw_surface_on_surface(spritesheet, enemy_list[i][j], (0, 0), (37, 11, 16, 8))
     previous_second = current_second
 
-    SCREEN.fill((0, 0, 0))
-    SCREEN.blit(player, (player_position.x, player_position.y))
+    fill_surface_with_color(SCREEN, (0, 0, 0))
+    draw_surface_on_surface(player, SCREEN, (player_position.x, player_position.y), (0, 0, 16, 8))
     for i in range(ENEMY_ROWS):
         for j in range(ENEMY_COLUMNS):
             # Check if enemy hit bullet
@@ -314,15 +267,15 @@ while game_running:
 
             # Display enemy if alive
             if enemy_timer_list[i][j] > 0:
-                SCREEN.blit(enemy_list[i][j], (enemy_positions_list[i][j].x, enemy_positions_list[i][j].y))
-    SCREEN.blit(bullet, (bullet_position.x, bullet_position.y))
+                draw_surface_on_surface(enemy_list[i][j], SCREEN, (enemy_positions_list[i][j].x, enemy_positions_list[i][j].y), (0, 0, 16, 8))
+    draw_surface_on_surface(bullet, SCREEN, (bullet_position.x, bullet_position.y), (0, 0, 1, 4))
 
     for i in range(28, 230):
         # Do not draw line at positions in linebreak_list to display destroyed areas
         if i in linebreak_list:
             continue
         # Draw line one pixel at a time
-        SCREEN.set_at((i, 15), (255, 255, 255))
-    pygame.display.flip()
+        draw_coloured_pixel_on_surface(SCREEN, (i, 15), (255, 255, 255))
+    update_display_surface()
 
-pygame.quit()
+quit_game()
