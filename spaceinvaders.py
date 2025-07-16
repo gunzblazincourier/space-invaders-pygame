4bf6e25106c7c2744bc7314a4ede11e1e137eb63
gunzblazincourier <gunzblazinAKM@proton.me>
2025-07-15 22:29:54 -0600

    Add documentation

--- spaceinvaders.py
+++ spaceinvaders.py
@@ -2,23 +2,30 @@ import pygame
 
 pygame.init()
 SCREEN = pygame.display.set_mode((256, 224), pygame.SCALED | pygame.FULLSCREEN)
-clock = pygame.time.Clock()
-spritesheet = pygame.image.load('sheet.png').convert()
 
 PLAYER_WIDTH = 16
 PLAYER_HEIGHT = 8
 player = pygame.Surface((PLAYER_WIDTH, PLAYER_HEIGHT)).convert()
+
+spritesheet = pygame.image.load('sheet.png').convert()
 player.blit(spritesheet, (0, 0), (1, 49, PLAYER_WIDTH, PLAYER_HEIGHT))
 player_position = pygame.Vector2(128, 200)
 
 enemy_direction = True  #True = right; False = left
 previous_second = 0
 current_second = 0
-
 ENEMY_ROWS, ENEMY_COLUMNS = (5, 11)
+
+# player_position assigned by default to set correct data type of Vector2 to values
 enemy_positions_list =  [[player_position for _ in range(ENEMY_COLUMNS)] for _ in range(ENEMY_ROWS)]
-enemy_check_list = [[False for _ in range(ENEMY_COLUMNS)] for _ in range(ENEMY_ROWS)]
+
+# Stores which enemies are dead
+enemy_death_list = [[False for _ in range(ENEMY_COLUMNS)] for _ in range(ENEMY_ROWS)]
+
+# Dead enemies play death animation until timer runs out
 enemy_timer_list = [[1.00 for _ in range(ENEMY_COLUMNS)] for _ in range(ENEMY_ROWS)]
+
+# Helps assign positions to enemies by updating values in these variables during below loop
 enemy_position_x = 40
 enemy_position_y = 60
 for i in range(ENEMY_ROWS):
@@ -27,7 +34,6 @@ for i in range(ENEMY_ROWS):
         enemy_position_x += 16
     enemy_position_x = 40
     enemy_position_y += 15
-print(enemy_positions_list)
 
 ENEMY_WIDTH = 16
 ENEMY_HEIGHT = 8
@@ -36,28 +42,28 @@ for i in range(ENEMY_ROWS):
     for j in range(ENEMY_COLUMNS):
         enemy = pygame.Surface((ENEMY_WIDTH, ENEMY_HEIGHT)).convert()
         enemy_list[i][j] = enemy
-print(enemy_list)
 
 BULLET_WIDTH = 1
 BULLET_HEIGHT = 4
 bullet = pygame.Surface((BULLET_WIDTH, BULLET_HEIGHT)).convert()
 bullet.blit(spritesheet, (0, 0), (55, 53, BULLET_WIDTH, BULLET_HEIGHT))
 gun_position = pygame.Vector2(136, 200)
-bullet_position = gun_position
+bullet_position = gun_position  # 'Stores' bullet in gun of spaceship by default
 
 sound_shoot = pygame.mixer.Sound('shoot.wav')
 channel_shoot = pygame.mixer.Channel(1)
 
-linebreak_list = []
+linebreak_list = [] # Line is broken at which locations
 bullet_shot = False
-running = True
-while running:
-    dt = clock.tick(144) / 1000
+game_running = True
+clock = pygame.time.Clock()
+while game_running:
+    delta_time = clock.tick(144) / 1000
     current_second = int(pygame.time.get_ticks() / 1000)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
+            game_running = False
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_SPACE] and bullet_shot == False:
@@ -65,36 +71,36 @@ while running:
         bullet_shot = True
         channel_shoot.play(sound_shoot)
     if keys[pygame.K_LEFT] and player_position.x > 30:
-        player_position.x -= 100 * dt
-        gun_position.x -= 100 * dt
+        player_position.x -= 100 * delta_time
+        gun_position.x -= 100 * delta_time
     elif keys[pygame.K_RIGHT] and player_position.x < 220:
-        player_position.x += 100 * dt
-        gun_position.x += 100 * dt
+        player_position.x += 100 * delta_time
+        gun_position.x += 100 * delta_time
 
+    # Bullet destroys line where it hits
     if bullet_position.y < 15:
         bullet_shot = False
         linebreak_list.append(int(bullet_position.x))
 
     if bullet_shot:
-        bullet_position.y -= 200 * dt
+        bullet_position.y -= 200 * delta_time
     else:
-        bullet_position = gun_position
+        bullet_position = gun_position  #  Bullet respawns at gun_position
 
     for i in range(ENEMY_ROWS):
         for j in range(ENEMY_COLUMNS):
-            if enemy_check_list[i][j]:
+            if enemy_death_list[i][j]:
                 if enemy_timer_list[i][j] > 0:
-                    enemy_timer_list[i][j] -= 5 * dt
+                    enemy_timer_list[i][j] -= 5 * delta_time    # Play dead enemy's timer for death animation
                     enemy_list[i][j].blit(spritesheet, (0, 0), (55, 1, ENEMY_WIDTH, ENEMY_HEIGHT))
                 else:
-                    enemy_timer_list[i][j] = 0
+                    enemy_timer_list[i][j] = 0  # Death animation finished, enemy removed
             else:
-                if previous_second != current_second:
+                if previous_second != current_second: # Means if one second has passed
                     if enemy_direction:
                         if enemy_positions_list[ENEMY_ROWS-1][ENEMY_COLUMNS-1].x < 220:
                             enemy_positions_list[i][j].x += 3
                         else:
-                            print("yes")
                             enemy_positions_list[i][j].y += 3
                             if (i == ENEMY_ROWS-1) and (j == ENEMY_COLUMNS-1):
                                 enemy_direction = False
@@ -103,11 +109,11 @@ while running:
                         if enemy_positions_list[ENEMY_ROWS-1][ENEMY_COLUMNS-1].x > 185:
                             enemy_positions_list[i][j].x -= 3
                         else:
-                            print("yes")
                             enemy_positions_list[i][j].y += 3
                             if (i == ENEMY_ROWS-1) and (j == ENEMY_COLUMNS-1):
                                 enemy_direction = True
 
+                # Animate enemies
                 if i == 0:
                     if current_second % 2 == 0:
                         enemy_list[i][j].blit(spritesheet, (0, 0), (1, 1, ENEMY_WIDTH, ENEMY_HEIGHT))
@@ -134,20 +140,24 @@ while running:
     SCREEN.blit(player, (player_position.x, player_position.y))
     for i in range(ENEMY_ROWS):
         for j in range(ENEMY_COLUMNS):
-            if bullet_shot == True and enemy_check_list[i][j] == False:
+            # Check if enemy hit bullet
+            if bullet_shot == True and enemy_death_list[i][j] == False:
                 if (enemy_positions_list[i][j].x + 3 < bullet_position.x <
                         enemy_positions_list[i][j].x + ENEMY_WIDTH - 3) and \
                         bullet_position.y < enemy_positions_list[i][j].y + ENEMY_HEIGHT:
-                    enemy_check_list[i][j] = True
+                    enemy_death_list[i][j] = True
                     bullet_shot = False
+            # Display enemy if alive
             if enemy_timer_list[i][j] > 0:
                 SCREEN.blit(enemy_list[i][j], (enemy_positions_list[i][j].x, enemy_positions_list[i][j].y))
     SCREEN.blit(bullet, (bullet_position.x, bullet_position.y))
 
     for i in range(28, 230):
+        # Do not draw line at positions in linebreak_list to display destroyed areas
         if i in linebreak_list:
             continue
         #pygame.draw.line(SCREEN, (255, 255, 255), (i, 15), (i, 15))
+        # Draw line one pixel at a time
         SCREEN.set_at((i, 15), (255, 255, 255))
     pygame.display.flip()
 
